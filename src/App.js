dc3a3181a0615e73dad4d599cbb38c24030c95fa
Carlos Hurtaado <carloshurtado219@yahoo.com>
2019-10-12 16:44:48 -0700

    Basic Signup Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
 import RecipeDetails from './components/recipes/RecipeDetails';
+import SignIn from './components/auth/SignIn';
 
 import './App.css';
 
@@ -17,6 +18,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={ Dashboard } />
             <Route path='/recipe/:id' component={ RecipeDetails } />
+            <Route path='/signin' component={ SignIn } />
           </Switch>
         </div>
       </BrowserRouter>
